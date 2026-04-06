d1402a6f260b5f37b1dfca632fd6f75df5bb29a6
Nikolay Beketov <nbek@miko.ru>
2026-04-06 14:12:51 +0700

    Initial commit: ModuleUkrainianLanguagePack language pack for MikoPBX

---
+++ Messages/en/ModuleUkrainianLanguagePack.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'BreadcrumbModuleUkrainianLanguagePack' => 'Ukrainian Language Pack',
+    'SubHeaderModuleUkrainianLanguagePack' => 'Complete Ukrainian language support for MikoPBX',
+];
---
+++ Messages/ru/ModuleUkrainianLanguagePack.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'BreadcrumbModuleUkrainianLanguagePack' => 'Языковой пакет - Украинский',
+    'SubHeaderModuleUkrainianLanguagePack' => 'Комплексная поддержка украинского языка для системы MikoPBX',
+];
